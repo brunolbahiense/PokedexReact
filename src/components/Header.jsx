5a88ea05c5460f5bdbf9300eaed6fcc0748f9329
BrunolBahiense <brunoluisbahiense@gmail.com>
2020-10-27 14:42:24 -0300

    projeto realizado em React

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.css'
+
+export default () =>
+        <header>
+            <img className="dexIcon" src="../pokeball.svg" alt="dexIcon"></img>
+            <h1> Pokedex</h1>
+        </header>
